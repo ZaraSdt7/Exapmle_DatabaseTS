285749ce52c91e7431ae403eccdd1d38f6cb4cc3
zahra <zahra.st7373@gmail.com>
2024-06-04 10:54:55 +0330

    ok

---
+++ src/generictype.ts
@@ -0,0 +1,22 @@
+type User = {
+    id:number
+    name:string
+    age:number
+}
+
+function mainuser<T extends User , K extends keyof T>(obj:T , keys:K[])
+{
+    let objuser:any = {};
+    for(const key of keys)
+        {
+            objuser[key] = obj[key]
+        }
+        return objuser
+
+}
+const Eployee:User = {
+    id:21,
+    name:"zara",
+    age:8
+}
+console.log(mainuser(Eployee,["name","id"]));
